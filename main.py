50aa1bd19487951628fc5286ea9348b3418185f8
Omar Valladolid <tliltik_kuauhtli@hotmail.com>
2025-08-17 22:44:09 +0100

    Starting to add text elements

--- main.py
+++ main.py
@@ -3,27 +3,51 @@ Main structure to control program
 """
 
 # Third-party
-import pygame
+import pygame as pg
 
-
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
+pg.init()
+screen = pg.display.set_mode((1280, 720))
+pg.display.set_caption("Inverse Kinematics - Cyclic Coordinate Descent")
+clock = pg.time.Clock()
 running = True
+delta_time = 0
+
+# Create background
+background = pg.Surface(screen.get_size())
+background = background.convert()
+# Background color
+background.fill((0, 0, 0))
+
+if not pg.font:
+    print("Warning, fonts disabled.")
+else:
+    font = pg.font.Font(None, 25)
+    # Text color
+    text = font.render("Body data:", True, (255, 255, 255))
+    text_pos = text.get_rect(x=10, y=10)
+    background.blit(text, text_pos)
+
+
+# Display background
+screen.blit(background, (0, 0))
+pg.display.flip()
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
 
     # Clear screen
     screen.fill("black")
 
     # More instructions here
+    # pg.draw
 
-    pygame.display.flip()
+    screen.blit(background, (0, 0))
+    pg.display.flip()
 
     # Set FPS=60
-    clock.tick(60)
+    # Delta time in seconds since last frame
+    delta_time = clock.tick(60) / 1000
 
-pygame.quit()
+pg.quit()
